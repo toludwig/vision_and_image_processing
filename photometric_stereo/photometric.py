a41478c91960202179752028f53dc69d6b16c4a3
toludwig <toludwig@uos.de>
2018-01-10 15:35:11 +0100

    buddha running

--- photometric_stereo/photometric.py
+++ photometric_stereo/photometric.py
@@ -21,21 +21,41 @@ def beethoven():
     N = M / albedo # normal field by normalizing M by albedo
     n1 = np.ones(np.shape(mask))
     n1[mask_coords] = N[0]
-    plt.imshow(n1)
-    plt.show()
     n2 = np.ones(np.shape(mask))
     n2[mask_coords] = N[1]
     n3 = np.ones(np.shape(mask))
     n3[mask_coords] = N[2]
     Z = ps.simchony_integrate(n1, n2, n3, mask)
     Z = np.nan_to_num(Z)
-    print(len(np.where(Z==0)[0]))
     ps.display_depth_matplotlib(Z)
 
 
-def buddha():
-    print(ps.read_data_file("Buddha.mat"))
 
+def buddha():
+    (I, mask, S) = ps.read_data_file("Buddha.mat")
+    mask_coords = np.where(mask != 0)
+    nz = len(mask_coords[0])
+    J = np.zeros((10, nz))
+    for i, (x,y) in enumerate(zip(*mask_coords)):
+        J[:,i] = I[x,y]
+    M = np.linalg.pinv(S) @ J # albedo modulated normal field
+    albedo = np.linalg.norm(M, axis=0)
+    alb_Im = np.zeros(np.shape(mask))
+    alb_Im[mask_coords] = albedo
+    plt.imshow(alb_Im, cmap="gray")
+    plt.show()
+
+    N = M / albedo # normal field by normalizing M by albedo
+    n1 = np.ones(np.shape(mask))
+    n1[mask_coords] = N[0]
+    n2 = np.ones(np.shape(mask))
+    n2[mask_coords] = N[1]
+    n3 = np.ones(np.shape(mask))
+    n3[mask_coords] = N[2]
+    Z = ps.simchony_integrate(n1, n2, n3, mask)
+    Z = np.nan_to_num(Z)
+    ps.display_depth_matplotlib(Z)
 
 if __name__ == "__main__":
-    beethoven()
+    #beethoven()
+    buddha()
